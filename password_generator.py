9a2f4e5f6658dcb28a11f95e7ec9a643fe62c023
Mohit Kumar Paul <95656083+1mohitpl@users.noreply.github.com>
2022-01-02 19:05:01 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,11 @@
+#build_password_generator_usuing_python.
+
 import random
 lower_value=("abcdefghujkohlshj")
 upper_value=("ABCDEFGHJIKLLJUTYRSXZV")
 symbols_value=("0123456789")
 Numbers_value=("{[]},.<>/?_")
-
 all= lower_value +upper_value + symbols_value + Numbers_value
-
 length= 10
-
 password= "".join(random.sample(all,length))
 print(password)
